819855ab5cdd7ba6d089fd9f8b38d7a5393c0e07
Brandon Lammey <archerlammey@ku.edu>
2017-09-11 09:37:41 -0500

    Created methods in header for exec
    Input methods with method descriptions into header file for class exec

--- exec.h
+++ exec.h
@@ -9,7 +9,27 @@ class exec
 
 	
 	public:
+		//constructor and destructor
+		exec();
+		~exec();
 
+		/* admin method controls admin options
+		* Option 1: Creates event (creates date object and prints to text file)
+		* Option 2: Look at event
+		*/
+		void admin();
+
+		/* user method controls user options
+		* Option 1: Check Active Events
+		* 	- Select Available Event (Update text file)
+		*/
+		void user();
+
+		/* print method creates linked list, accesses text file, creates date objects,
+		* and places date objects into sorted linked list
+		* Output: Print list in readable form to terminal (uses time parameter to determine 12/24 hr clock)
+		*/
+		void print(bool time);
 
 };
 #endif
