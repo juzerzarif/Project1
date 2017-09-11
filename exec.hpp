6db915defd9371fa1cfbf73925a9271d33f28f65
Dylan Herrig <dylan.herrig@gmail.com>
2017-09-11 10:21:41 -0500

    Eddited user method

--- exec.hpp
+++ exec.hpp
@@ -17,15 +17,39 @@ exec :: ~exec()
 
 void exec :: admin()
 	{
-
+		
 	}
 
 void exec :: user()
 	{
+		int choice;
+		bool hourclock = true;
+
+		std::cout << "Would you like your times displayed on a 12 hour or 24hour clock?\n"
+		std::cout << "1) 12 hour clock\n"
+		std::cout << "2) 24 hour clock\n"
+
+		std::cin >> choice;
+
+		if(choice == 1)
+		{
+			//print
+		}
+		else if(choice == 2)
+		{
+
+		}
+		else
+		{
+
+		}
+
+		//call to print method
+		std::cout << "Please choose an event from the list above";
 
 	}
 
 void exec :: print(bool time)
 	{
-
+		
 	}
