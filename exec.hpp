70f01df35cfc2c2be9d294b2b3f182518c996e8c
Dylan Herrig <dylan.herrig@gmail.com>
2017-09-11 09:52:15 -0500

    Added includes toe exec.hpp

--- exec.hpp
+++ exec.hpp
@@ -1,1 +1,21 @@
 //ADMIN AND USER METHODS
+
+#include "exec.h"
+#include "linkedList.h"
+#include <iostream>
+
+
+void exec :: admin()
+	{
+
+	}
+
+void exec :: user()
+	{
+
+	}
+
+void exec :: print(bool time)
+	{
+
+	}
