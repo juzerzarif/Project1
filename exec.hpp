74d5400eea232499cbeea05becbeea487de405a7
Giovaartavia <giovaartavia@hotmail.com>
2017-09-11 19:02:55 -0500

    Formatted admin method choices
    Admin method asks for the necessary choices and takes them in the correct types

--- exec.hpp
+++ exec.hpp
@@ -35,11 +35,86 @@ void exec :: run()
 		{
 			//error	
 		}
-    	}
+    }
 
 void exec :: admin()
 	{
+		/* 1. Name of event
+		* 2. Year
+		* 3. Month
+		* 4. Day
+		* 5. Time
+		* 6. Number of people attending (1)*/
+
+		int choice = 0;
+		int eventYear = 0;
+		int eventMonth = 0;
+		int eventDay = 0;
+		int halfInterval = 0;
+		int peopleAttending = 1;
+		std::string activityName;
+		std::string eventTime;
+
+		std::cout << "Select an option:\n";
+		std::cout << "1) Create new activity\n";
+		std::cout << "2) View current activities\n";
 		
+		std::cin >> choice;
+
+		if(choice == 1)
+		{
+			std::cout << "Please enter the name of the activity:\n";
+			std::cin.ignore();
+			std::getline (std::cin,activityName,'\n');
+
+			std::cout << "What year will the event be on?\n";
+			std::cin >> eventYear;
+
+			std::cout << "What month will the event be on?:\n";
+			std::cout << "1) January\n";
+			std::cout << "2) February\n";
+			std::cout << "3) March\n";
+			std::cout << "4) April\n";
+			std::cout << "5) May\n";
+			std::cout << "6) June\n";
+			std::cout << "7) July\n";
+			std::cout << "8) August\n";
+			std::cout << "9) September\n";
+			std::cout << "10) October\n";
+			std::cout << "11) November\n";
+			std::cout << "12) December\n \n";
+			std::cin >> eventMonth;
+
+			std::cout << "What day will you event be on?\n";
+			std::cin >> eventDay; //TODO: Check if valid day.
+
+			std::cout << "At what time will your event take place? (Time is meassured in half hour intervals. If your event spans multiple half hours, please enter the initial half hour interval.)\n";
+			std::cout << "Examples: 1100 indicates 11 a.m. 2130 indicates 9:30 p.m.\n";
+			std::cin.ignore();
+			std::getline (std::cin,eventTime,'\n');
+
+			std::cout << "Does your event span another half hour interval?\n";
+			std::cout << "1) Yes\n";
+			std::cout << "2) No\n";
+			std::cin >> halfInterval; //TODO check if valid time
+
+			while(halfInterval == 1)
+			{
+				std::string additionalTime = "";
+				std::cout << "What other interval of time will your event be taking? (Time is meassured in half hour intervals. If your event spans multiple half hours, please enter the initial half hour interval.)\n";
+				std::cout << "Examples: 1100 indicates 11 a.m. 2130 indicates 9:30 p.m.\n";
+				std::cin.ignore();
+				std::getline (std::cin,additionalTime,'\n');
+				eventTime = eventTime + " " + additionalTime;
+
+				std::cout << "Does your event span another half hour interval?\n";
+				std::cout << "1) Yes\n";
+				std::cout << "2) No\n";
+				std::cin >> halfInterval;
+			}
+			//std::cout << eventTime;
+		}
+	
 	}
 
 void exec :: user()
